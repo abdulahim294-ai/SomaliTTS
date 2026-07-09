3146fe6bdff91cb61adc413de2e2ee5b7099f503
abdulahim294-ai <Abdulahim294@gmail.com>
2026-07-09 20:01:52 +0300

    add real tts engine

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
+from TTS.api import TTS
+import uuid
 
 app = FastAPI()
+
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
 
 @app.get("/")
 def home():
     return {"status": "SomaliTTS running"}
 
 @app.post("/tts")
-def tts(text: str = Form(...)):
-    return {
-        "message": "Received text",
-        "length": len(text)
-    }
+def generate(text: str = Form(...)):
+
+    filename = f"{uuid.uuid4()}.wav"
+
+    tts.tts_to_file(
+        text=text,
+        file_path=filename
+    )
+
+    return FileResponse(
+        filename,
+        media_type="audio/wav",
+        filename="somali_voice.wav"
+    )
