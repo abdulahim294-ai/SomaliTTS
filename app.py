b925a13da94f8c91a132ff4f83c4a16d91ad7c78
abdulahim294-ai <Abdulahim294@gmail.com>
2026-07-09 21:52:28 +0300

    optimize tts loading

--- app.py
+++ app.py
@@ -1,31 +1,42 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
-from TTS.api import TTS
 import uuid
+import os
 
 app = FastAPI()
 
 tts = None
 
+
 @app.get("/")
 def home():
-    return {"status": "SomaliTTS running"}
+    return {
+        "status": "SomaliTTS running",
+        "service": "online"
+    }
+
 
 @app.post("/tts")
 def generate(text: str = Form(...)):
     global tts
 
     if tts is None:
-        tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
+        from TTS.api import TTS
+        tts = TTS(
+            "tts_models/multilingual/multi-dataset/xtts_v2",
+            gpu=False
+        )
 
-    filename = f"{uuid.uuid4()}.wav"
+    filename = f"/tmp/{uuid.uuid4()}.wav"
 
     tts.tts_to_file(
         text=text,
-        file_path=filename
+        file_path=filename,
+        language="en"
     )
 
     return FileResponse(
         filename,
-        media_type="audio/wav"
+        media_type="audio/wav",
+        filename="output.wav"
     )
