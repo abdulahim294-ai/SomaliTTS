7c63312862c7b4384dbac1fdd451e08265507276
abdulahim294-ai <Abdulahim294@gmail.com>
2026-07-11 16:53:43 +0300

    fix app structure

--- app.py
+++ app.py
@@ -1,39 +1,59 @@
+from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse
+from pydub import AudioSegment
+import edge_tts
+import asyncio
+import uuid
+import os
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "status": "SomaliTTS running"
+    }
+
+
 @app.post("/tts")
 def generate(text: str = Form(...)):
-    import uuid
-    from pydub import AudioSegment
-    import edge_tts
-    import asyncio
-    import os
 
-    parts = [text[i:i+2500] for i in range(0, len(text), 2500)]
+    parts = [
+        text[i:i+2500]
+        for i in range(0, len(text), 2500)
+    ]
 
-    output_files = []
+    files = []
 
-    async def create_audio():
+    async def create():
         for i, part in enumerate(parts):
             filename = f"/tmp/part_{i}.mp3"
 
-            communicate = edge_tts.Communicate(
+            voice = edge_tts.Communicate(
                 part,
                 "so-SO-UbaxNeural"
             )
 
-            await communicate.save(filename)
-            output_files.append(filename)
+            await voice.save(filename)
+            files.append(filename)
 
-    asyncio.run(create_audio())
+    asyncio.run(create())
 
-    final = AudioSegment.empty()
+    final_audio = AudioSegment.empty()
 
-    for f in output_files:
-        final += AudioSegment.from_mp3(f)
+    for file in files:
+        final_audio += AudioSegment.from_mp3(file)
 
-    result = f"/tmp/{uuid.uuid4()}.mp3"
-    final.export(result, format="mp3")
+    output = f"/tmp/{uuid.uuid4()}.mp3"
+
+    final_audio.export(
+        output,
+        format="mp3"
+    )
 
     return FileResponse(
-        result,
+        output,
         media_type="audio/mpeg",
         filename="somali_voice.mp3"
     )
